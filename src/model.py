d07c531091e6c401d76fd2446af65b703568bbd5
jasonfreak <jasonfreak@126.com>
2016-05-12 22:37:43 +0800

    1>rename some columns; 2>fix some bugs

--- src/model.py
+++ src/model.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_selection import RFE
 from sklearn.pipeline import Pipeline
 from sklearn.externals.joblib import dump, load
 from base import BASE_MODEL, FEATURES
@@ -15,8 +16,9 @@ def _getEncoderList(isCategoryList):
 def init():
     step1 = ('OneHotEncoder', OneHotEncoder(sparse=False, handle_unknown='ignore', categorical_features=_getEncoderList(FEATURES.values())))
     step2 = ('StandardScaler', StandardScaler())
-    step3 = ('model', BASE_MODEL())
-    pipeline = Pipeline(steps=[step1, step2, step3])
+    #step3 = ('RFE', RFE(estimator=BASE_MODEL(), n_features_to_select=20))
+    step4 = ('model', BASE_MODEL)
+    pipeline = Pipeline(steps=[step1, step2, step4])
     dump(pipeline, 'dump/model', compress=3)
 
 def fit():
@@ -24,7 +26,17 @@ def fit():
     print '[X]', X[0]
     pipeline = load('dump/model')
     pipeline.fit(X, y)
-    print '[fit]', pipeline.get_params()['model'].feature_importances_
+    featureNameList = FEATURES.keys()
+    model = pipeline.get_params()['model']
+    coefList = None
+    if hasattr(model, 'feature_importances_'):
+        coefList = model.feature_importances_
+    elif hasattr(model, 'coef_'):
+        coefList = model.coef_[0]
+    if coefList is not None:
+        indexList = sorted(range(len(featureNameList)), key=lambda x:coefList[x], reverse=True)
+        for index in indexList:
+            print '[fit] NAME[%40s], COEF[%.8f]' % (featureNameList[index], coefList[index])
     dump(pipeline, 'dump/model', compress=3)
 
 def predict():
@@ -42,6 +54,7 @@ def predict():
     real_score =  np.dot(1-std, weight)
     ideal_score = np.sum(weight)
     print '[random_score]', np.dot(np.random.uniform(0, 1, size=n_artists), weight)
+    print '[average_score]', np.dot(np.ones(n_artists) / 2, weight)
     print '[excellent_score]', np.dot(1-np.ones(n_artists)/10, weight)
     print '[real_score]', real_score
     print '[ideal_score]', ideal_score, real_score / ideal_score
