2e99bd7f0beacc9e710bc194ae99e0baf57cf82b
jasonfreak <jasonfreak@126.com>
2016-05-18 11:51:40 +0800

    change to rf

--- src/base.py
+++ src/base.py
@@ -7,7 +7,7 @@ from sklearn.tree import DecisionTreeRegressor
 ISOFFLINE = True
 HOST = 'almsc'
 USER = 'root'
-PASSWORD = 'Almsc@2016'
+PASSWORD = 'Contest@2016'
 if ISOFFLINE:
     print '[-------------------------O F F L I N E-------------------------]'
     DATABASE = 'almsc_offline'
@@ -16,9 +16,9 @@ else:
     DATABASE = 'almsc_online'
 TIME_FORMAT='%Y%m%d'
 N_SERIES_DAYS=15
-BASE_MODEL=LinearSVR()
+#BASE_MODEL=LinearSVR()
 #BASE_MODEL=DecisionTreeRegressor()
-#BASE_MODEL=RandomForestRegressor()
+BASE_MODEL=RandomForestRegressor()
 #BASE_MODEL=GradientBoostingRegressor()
 if isinstance(BASE_MODEL, LinearSVR):
     GRIDPARAMS = {
@@ -27,9 +27,10 @@ if isinstance(BASE_MODEL, LinearSVR):
     }
 elif isinstance(BASE_MODEL, RandomForestRegressor):
     GRIDPARAMS = {
-    'model__n_estimators':np.arange(1, 20),
-    'model__min_samples_split':np.arange(2, 51), 
-    'model__min_samples_leaf':np.arange(1, 51),
+#    'model__n_estimators':np.arange(1, 20),
+    'model__n_estimators':[100],
+#    'model__min_samples_split':np.arange(2, 51), 
+#    'model__min_samples_leaf':np.arange(1, 51),
     }
 elif isinstance(BASE_MODEL, GradientBoostingRegressor):
     GRIDPARAMS = {
@@ -43,7 +44,7 @@ RF_MODEL=LinearSVR(C=0.05)
 N_SELECTED_FEATURES=0
 FEATURES = {
 #artist
-#'r_artist_code':True,
+'r_artist_code':True,
 'r_n_songs':False,
 'r_gender':True,
 'r_n_languages':False,
@@ -55,37 +56,37 @@ FEATURES = {
 'r_avg_plays_last_3_days':False,
 'r_avg_plays_last_5_days':False,
 'r_avg_plays_last_7_days':False,
-#'r_q2_plays_div_q1_plays':False,
-#'r_q3_plays_div_q2_plays':False,
-#'r_q4_plays_div_q3_plays':False,
+'r_q2_plays_div_q1_plays':False,
+'r_q3_plays_div_q2_plays':False,
+'r_q4_plays_div_q3_plays':False,
 'r_sum_plays':False,
 'r_sum_downloads':False,
 'r_sum_collects':False,
 'r_sum_downloads_div_sum_plays':False,
 'r_sum_collects_div_sum_plays':False,
-#'r_q2_div_q1_mul_avg_plays':False,
-#'r_q3_div_q2_mul_avg_plays':False,
-#'r_q4_div_q3_mul_avg_plays':False,
-#'r_q2_div_q1_mul_plays':False,
-#'r_q3_div_q2_mul_plays':False,
-#'r_q4_div_q3_mul_plays':False,
-#'r_q3_q2_q1_acceleration':False,
-#'r_q4_q3_q2_acceleration':False,
-#'r_normal_q2_plays_diff_q1_plays':False,
-#'r_normal_q3_plays_diff_q2_plays':False,
-#'r_normal_q4_plays_diff_q3_plays':False,
+'r_q2_div_q1_mul_avg_plays':False,
+'r_q3_div_q2_mul_avg_plays':False,
+'r_q4_div_q3_mul_avg_plays':False,
+'r_q2_div_q1_mul_plays':False,
+'r_q3_div_q2_mul_plays':False,
+'r_q4_div_q3_mul_plays':False,
+'r_q3_q2_q1_acceleration':False,
+'r_q4_q3_q2_acceleration':False,
+'r_normal_q2_plays_diff_q1_plays':False,
+'r_normal_q3_plays_diff_q2_plays':False,
+'r_normal_q4_plays_diff_q3_plays':False,
 #day
 #'r_month':True,
-#'r_day':True,
+'r_day':True,
 #'r_season':True,
 #'r_week':True,
 'r_weekday':True,
 'r_is_holiday':False,
 'r_n_holidays':False,
 'r_i_holidays':True,
-#'r_is_good_voice':False,
-#'r_is_music_festival':False,
-#'r_diff_days':True,
+'r_is_good_voice':False,
+'r_is_music_festival':False,
+'r_diff_days':True,
 #artist & day
 'r_avg_plays_weekday':False,
 #series
