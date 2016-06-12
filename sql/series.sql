5c607f9203d26adaa183fa2ea5ca018c2aa149b7
jasonfreak <jasonfreak@126.com>
2016-06-12 22:02:29 +0800

    fix series.sql

--- sql/series.sql
+++ sql/series.sql
@@ -8,7 +8,7 @@ val float,
 primary key(artist_id, ds, name)
 );
 insert into mars_tianchi_series(artist_id, ds, name) select mars_tianchi_artists.artist_id, mars_tianchi_ds.ds, 's_plays' as name from mars_tianchi_artists, mars_tianchi_ds;
-update mars_tianchi_series left join mars_tianchi_artist_actions on mars_tianchi_series.artist_id = mars_tianchi_artist_actions.artist_id and mars_tianchi_series.ds = mars_tianchi_artist_actions.ds and mars_tianchi_artist_actions.action_type = '1' set mars_tianchi_series.val = mars_tianchi_artist_actions.n where mars_tianchi_series.name = 's_plays';
+update mars_tianchi_series left join mars_tianchi_artist_actions on mars_tianchi_series.artist_id = mars_tianchi_artist_actions.artist_id and mars_tianchi_series.ds = mars_tianchi_artist_actions.ds and mars_tianchi_artist_actions.action_type = '1' set mars_tianchi_series.val = ifnull(mars_tianchi_artist_actions.n, 0) where mars_tianchi_series.name = 's_plays';
 --select * from mars_tianchi_series where name = 's_new_plays_div_plays'
 --select distinct name from mars_tianchi_series where val is null
 --delete from mars_tianchi_series where name = 's_downloads_div_plays_last_7_days'
